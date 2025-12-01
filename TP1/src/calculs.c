24319d8dea69b706e4b3d163627c304fd68438c2
FasilDev <144022305+FasilDev@users.noreply.github.com>
2025-12-01 15:31:38 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+
+    int num1 = 10;
+    int num2 = 3;
+    char op = '&';   // change selon le test : + - * / % & | ~
+
+    int resultat;
+
+    switch (op) {
+
+        case '+':
+            resultat = num1 + num2;
+            printf("Resultat : %d\n", resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("Resultat : %d\n", resul
