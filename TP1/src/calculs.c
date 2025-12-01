35cc3c13c55401442a58e62f77c7e4c3146f611a
FasilDev <144022305+FasilDev@users.noreply.github.com>
2025-12-01 15:32:56 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -17,4 +17,51 @@ int main() {
 
         case '-':
             resultat = num1 - num2;
-            printf("Resultat : %d\n", resul
+            printf("Resultat : %d\n", resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("Resultat : %d\n", resultat);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Erreur : division par zero !\n");
+            } else {
+                resultat = num1 / num2;
+                printf("Resultat : %d\n", resultat);
+            }
+            break;
+
+        case '%':
+            if (num2 == 0) {
+                printf("Erreur : modulo par zero !\n");
+            } else {
+                resultat = num1 % num2;
+                printf("Resultat : %d\n", resultat);
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("ET bit-a-bit : %d\n", resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("OU bit-a-bit : %d\n", resultat);
+            break;
+
+        case '~':
+            resultat = ~num1;  // opérateur unaire → s'applique à num1 seulement
+            printf("NON bit-a-bit (~num1) : %d\n", resultat);
+            break;
+
+        default:
+            printf("Operateur inconnu.\n");
+            break;
+    }
+
+    return 0;
+}
