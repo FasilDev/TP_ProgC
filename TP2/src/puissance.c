c6f2b4a7f11a856bbba728223f7253e2d084de45
FasilDev <144022305+FasilDev@users.noreply.github.com>
2025-12-01 15:37:35 +0100

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;   // base
+    int b = 3;   // exposant
+    int resultat = 1;
+
+    // Cas spécial : b = 0 → a^0 = 1
+    if (b == 0) {
+        resultat = 1;
+    } else {
+        for (int i = 1; i <= b; i++) {
+            resultat *= a;
+        }
+    }
+
+    printf("%d^%d = %d\n", a, b, resultat);
+
+    return 0;
+}
